4983988734673dce7797050fd6c4ce3c259aa96f
slickthenick <nickrathman@gmail.com>
2014-08-16 00:45:31 -0700

    added default conf files to install folder

---
+++ tokenly-install-files/conf/api.php
@@ -0,0 +1,33 @@
+<?php
+define('CAPTCHA_PUB', ''); //recaptcha public key
+define('CAPTCHA_PRIV', ''); //recaptcha private key
+
+define('SOUNDCLOUD_ID', ''); //soundcloud client ID
+define('SOUNDCLOUD_SECRET', ''); //soundcloud secret key
+
+define('XCP_USER', ''); //counterparty RPC user
+define('XCP_PASS', ''); //counterparty RPC password
+define('XCP_IP', ''); //counterparty IP:port
+define('XCP_CONNECT', 'http://'.XCP_USER.':'.XCP_PASS.'@'.XCP_IP.'/api/');
+define('XCP_WALLET', ''); //counterparty/bitcoin wallet pass
+define('XCP_PREFIX', 'XCP_'); //prefix for counterparty related accounts on bitcoind
+define('SATOSHI_MOD', 100000000); //100 million satoshis = 1 BTC
+define('XCP_BASE_FEE', 10000); //in satoshis.. default base fee to use on transactions
+define('XCP_FEE_MOD', (10860 * 2)); //default dust input size x2 for counterparty transactions
+
+define('BTC_USER', ''); //bitcoind RPC user
+define('BTC_PASS', ''); //bitcoind RPC password
+define('BTC_IP', ''); //bitcoind IP:port
+define('BTC_CONNECT', 'http://'.BTC_USER.':'.BTC_PASS.'@'.BTC_IP.'/');
+
+//disqus API credentials
+define('DISQUS_PUBLIC', '');
+define('DISQUS_SECRET', '');
+define('DISQUS_DEFAULT_FORUM', '');
+define('DISQUS_ADMIN', '');
+
+//amazon AWS API credentials
+define('AWS_ACCESS_KEY', '');
+define('AWS_SECRET_KEY', '');
+define('AWS_CONFIG_PATH', SITE_PATH.'/resources/aws/Aws/Common/Resources/aws-config.php');
+define('AWS_DEFAULT_BUCKET', '');
---
+++ tokenly-install-files/conf/config.php
@@ -0,0 +1,23 @@
+<?php
+ini_set('display_errors', 1); //toggle PHP debugging
+define('SITE_BASE', '/var/www/html'); //base path to installation, typically one level up from web root
+define('SITE_PATH', SITE_BASE.'/www'); //web root
+define('FRAMEWORK_PATH', SITE_PATH.'/slick');
+define('SITE_NAME', ''); //default system name (not important)
+define('SITE_DOMAIN', ''); //default system domain (without http://)
+
+//mysql credentials
+define('MYSQL_DB', '');
+define('MYSQL_USER', '');
+define('MYSQL_PASS', '');
+define('MYSQL_HOST', '');
+
+define('DATE_FORMAT', 'F j\, Y \a\t g:i A'); //default display date formatting
+
+define('PRIMARY_TOKEN_FIELD', 12); //fieldId for main token profile field (LTBcoin address)
+
+define('ENCRYPT_KEY', ''); //encryption key for encrypting some data into database
+
+if(file_exists(SITE_BASE.'/conf/api.php')){
+	include(SITE_BASE.'/conf/api.php');
+}
