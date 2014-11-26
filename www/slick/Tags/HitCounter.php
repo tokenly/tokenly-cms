bd2dc4dff6f190497c32267776589bb30ba8ee93
slickthenick <nickrathman@gmail.com>
2014-11-25 20:46:33 -0800

    added Hit Counter page tag

---
+++ www/slick/Tags/HitCounter.php
@@ -0,0 +1,41 @@
+<?php
+class Slick_Tags_HitCounter
+{
+	public $params = array();
+	
+	function __construct()
+	{
+		$this->model = new Slick_Core_Model;
+	}
+	
+	public function display()
+	{
+		$output = '';
+		$prefix = '';
+		if(isset($this->params['prefix'])){
+			$prefix = trim($this->params['prefix']);
+		}
+		$statKey = $prefix.'_hits';
+		$getStat = $this->model->get('stats', $statKey, array(), 'statKey');
+		$hits = 0;
+		if($getStat){
+			$hits = intval($getStat['statValue']);
+		}
+		
+		if(!isset($_SESSION['has_hit_'.$prefix])){
+			$newHits = $hits+1;
+			if($getStat){
+				$this->model->edit('stats', $getStat['statId'], array('statValue' => $newHits));
+			}
+			else{
+				$this->model->insert('stats', array('statKey' => $statKey, 'statValue' => $newHits));
+			}
+			$_SESSION['has_hit_'.$prefix] = true;
+		}
+		
+		$output = '<div class="hit-counter"><strong>'.$hits.' <span>'.pluralize('Visit', $hits, true).'</span></strong></div>';
+		
+		return $output;
+	}
+	
+}
