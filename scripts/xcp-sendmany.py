078abbd3a82047aa15061759fab713221add3941
cryptonaut <nrathman@ironcladtech.ca>
2015-04-14 11:30:00 -0700

    updated distributor script

--- scripts/xcp-sendmany.py
+++ scripts/xcp-sendmany.py
@@ -10,20 +10,28 @@ import requests
 RPC_USER = ''                                                                
 RPC_PASSWORD = ''                                                                 
 RPC_HOST = ''                                                          
-RPC_PORT = 4000                                            
-                                                                                
-                                                                                
-json_print = lambda x: print(json.dumps(x, sort_keys=True, indent=4))           
+RPC_PORT = 4000                   
+
+BTC_USER = ''
+BTC_PASSWORD = ''
+BTC_HOST = ''
+BTC_PORT = 8332                         
+
+#json_print = lambda x: print(json.dumps(x, sort_keys=True, indent=4))           
+
 headers = {'content-type': 'application/json'}                                  
                                                                                 
                                                                                 
-def api(payload):                                                               
-    host = 'http://{}:{}@{}:{}'.format(RPC_USER, RPC_PASSWORD, RPC_HOST, RPC_PORT)
+def api(payload, rpc, password, host, port):                                                               
+    host = 'http://{}:{}@{}:{}'.format(rpc, password, host, port)
     response = requests.post(host, data=json.dumps(payload), headers=headers)   
     try:                                                                        
         return response.json()['result']                                        
-    except KeyError:                                                            
-        print(response.json()['error'])                                         
+    except KeyError: 
+        try:
+            print(response.json()['data'])                                             
+        except KeyError:
+            print(response.json()['error'])
         return False                                                            
                                                                                 
 with open(sys.argv[1], 'r') as csvfile:                                         
@@ -35,33 +43,33 @@ with open(sys.argv[1], 'r') as csvfile:
         # Create send.                                                          
         payload = {                                                             
             "method": "create_send",                                            
-            "params": {'source': source, 'destination': destination, 'asset': asset, 'quantity': int(quantity), 'fee': int(fee), 'allow_unconfirmed_inputs': True, 'regular_dust_size': int(sys.argv[2]), 'multisig_dust_size': int(sys.argv[2])},
+            "params": {'source': source, 'destination': destination, 'asset': asset, 'quantity': int(quantity), 'fee': int(fee), 'allow_unconfirmed_inputs': True, 'regular_dust_size': int(sys.argv[2]), 'multisig_dust_size': int(sys.argv[2]), 'pubkey': sys.argv[3], 'op_return_value': int(sys.argv[2]), 'encoding': 'multisig'},
             "jsonrpc": "2.0",                                                   
             "id": 0                                                             
-        }   
-        #raise Exception(payload)                                         
-        unsigned_tx = api(payload)                                              
+        }
+                                  
+        unsigned_tx = api(payload, RPC_USER, RPC_PASSWORD, RPC_HOST, RPC_PORT)                                              
         if not unsigned_tx: continue                                            
-                                                                                
+                                                                               
         # Sign tx.                                                              
         payload = {                                                             
-            "method": "sign_tx",                                                
-            "params": {'unsigned_tx_hex': unsigned_tx},                         
+            "method": "signrawtransaction",                                                
+            "params": [unsigned_tx],                         
             "jsonrpc": "2.0",                                                   
             "id": 0                                                             
         }                                                                       
-        signed_tx = api(payload)                                                
+        signed_tx = api(payload, BTC_USER, BTC_PASSWORD, BTC_HOST, BTC_PORT)                                                
+
         if not signed_tx: continue                                              
                                                                                 
         # Broadcast tx.                                                         
         payload = {                                                             
-            "method": "broadcast_tx",                                           
-            "params": {'signed_tx_hex': signed_tx},                             
+            "method": "sendrawtransaction",                                           
+            "params": [signed_tx['hex']],                             
             "jsonrpc": "2.0",                                                   
             "id": 0                                                             
         }                                                                       
-        tx_hash = api(payload)                                                  
+        tx_hash = api(payload, BTC_USER, BTC_PASSWORD, BTC_HOST, BTC_PORT)                                                  
         print('Transaction', tx_hash)                                           
-                                                                                
-                                                                                
+                                                                     
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
