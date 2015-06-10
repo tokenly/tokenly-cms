d4d2c3942c03592276bc0d995c3551b0e3cd3989
cryptonaut <nrathman@ironcladtech.ca>
2015-06-10 00:56:12 -0700

    applied fix to tokenslot-client

--- slick/API/TokenSlotClient.php
+++ slick/API/TokenSlotClient.php
@@ -27,6 +27,9 @@ class TokenSlotClient
 		$url = $this->apiURL;
 		$params = array();
 		foreach($args as $key => $val){
+			if(is_array($val)){
+				$val = json_encode($val);
+			}			
 			$params[] = $key.'='.urlencode($val);
 			$args[$key] = urlencode($val);
 		}
