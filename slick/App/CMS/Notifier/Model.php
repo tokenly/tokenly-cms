52542b49a861a869348dfe89ed3dce872129ed22
cryptonaut <nrathman@ironcladtech.ca>
2015-10-05 19:25:28 -0700

    updated notification pusher module to be able to send to token holders, users with a registered address, or an individual username

--- slick/App/CMS/Notifier/Model.php
+++ slick/App/CMS/Notifier/Model.php
@@ -1,6 +1,6 @@
 <?php
 namespace App\CMS;
-use Core, UI, Util;
+use Core, UI, Util, App\Tokenly\AssetScout_Model;
 class Notifier_Model extends Core\Model
 {
 	public function getNotifyForm()
@@ -16,6 +16,11 @@ class Notifier_Model extends Core\Model
 		$groups->setLabel('Groups to Notify');
 		$groups->setLabelDir('R');
 		$form->add($groups);
+		
+		$token = new UI\Textbox('token');
+		$token->setLabel('OR notify token holders, bitcoin address or username');
+		$token->addAttribute('placeholder', 'Asset name, bitcoin address or username');
+		$form->add($token);
 		
 		$message = new UI\Textarea('message');
 		$message->addAttribute('required');
@@ -34,45 +39,68 @@ class Notifier_Model extends Core\Model
 			throw new \Exception('Please enter a message');
 		}
 		
-		if(!isset($data['groups']) OR !is_array($data['groups']) OR count($data['groups']) == 0){
-			throw new \Exception('Please select at least one group');
-		}
-		
-		if(in_array(0, $data['groups'])){
-			$sendAll = true;
-		}
-		else{
-			$sendAll = false;
-		}
-		
-		foreach($data['groups'] as $gKey => $groupId){
-			$group = array('groupId' => $groupId);
-			$groupUsers = $this->getAll('group_users', array('groupId' => $groupId));
-			$group['users'] = array();
-			foreach($groupUsers as $guser){
-				$group['users'][] = $guser['userId'];
-			}
-			$data['groups'][$gKey] = $group;
-			
-		}		
-		
-		$users = $this->getAll('users', array(), array('userId'));
 		$sendUsers = array();
-		foreach($users as $user){
-			if($sendAll){
-				$sendUsers[] = $user['userId'];
-			}
-			else{
-				foreach($data['groups'] as $group){
-					if(in_array($user['userId'], $group['users'])){
-						$sendUsers[] = $user['userId'];
+		if(isset($data['token']) AND trim($data['token']) != ''){
+			$scout = new AssetScout_Model;
+			$scoutAsset = $scout->scoutAsset(array('asset' => $data['token']));
+			if(is_array($scoutAsset)){
+				if($scoutAsset['isUser']){
+					$sendUsers[] = $scoutAsset['user']['userId'];
+				}
+				elseif($scoutAsset['isAddress']){
+					foreach($scoutAsset['users'] as $row){
+						$sendUsers[] = $row['userId'];
+					}
+				}
+				else{
+					//send to token holders
+					foreach($scoutAsset['list'] as $row){
+						$sendUsers[] = $row['userId'];
 					}
 				}
 			}
 		}
-		
+		else{
+			if(!isset($data['groups']) OR !is_array($data['groups']) OR count($data['groups']) == 0){
+				throw new \Exception('Please select at least one group');
+			}
+					
+			if(in_array(0, $data['groups'])){
+				$sendAll = true;
+			}
+			else{
+				$sendAll = false;
+			}
+			
+			foreach($data['groups'] as $gKey => $groupId){
+				$group = array('groupId' => $groupId);
+				$groupUsers = $this->getAll('group_users', array('groupId' => $groupId));
+				$group['users'] = array();
+				foreach($groupUsers as $guser){
+					$group['users'][] = $guser['userId'];
+				}
+				$data['groups'][$gKey] = $group;
+				
+			}		
+			
+			$users = $this->getAll('users', array(), array('userId'));
+			foreach($users as $user){
+				if($sendAll){
+					$sendUsers[] = $user['userId'];
+				}
+				else{
+					foreach($data['groups'] as $group){
+						if(in_array($user['userId'], $group['users'])){
+							$sendUsers[] = $user['userId'];
+						}
+					}
+				}
+			}
+					
+		}
+
 		if(count($sendUsers) == 0){
-			throw new \Exception('No users selected');
+			throw new \Exception('No users found');
 		}
 		
 		foreach($sendUsers as $userId){
