44b247b40f98c1067c5ea21cdad3f856bef5e207
cryptonaut <nrathman@ironcladtech.ca>
2015-10-05 18:47:01 -0700

    added several new custom functions for interacting with Bitcoin

--- slick/API/Bitcoin.php
+++ slick/API/Bitcoin.php
@@ -1,5 +1,6 @@
 <?php
 namespace API;
+use \Blocktrail\SDK\BlocktrailSDK, BitWasp\BitcoinLib\BitcoinLib, BitWasp\BitcoinLib\RawTransaction;
 /*
 					COPYRIGHT
 
@@ -59,6 +60,10 @@ class Bitcoin
 	private $notification = false;
 	
 	public $modify_request = true;
+	
+	public $min_fee = 0.00002;
+	public $dust_limit = 0.000055;
+	public $wallet_timeout = 300;
 	
 	protected static $utxoset = false;
 	
@@ -238,74 +243,316 @@ class Bitcoin
 		
 	}
 	
-	public function getaddressbalance($address)
+	public function getaddressbalance($address, $min_conf = 1)
 	{
-		if(!self::$utxoset){
-			$this->updateutxoset();
-		}
-		$unspent = self::$utxoset;
-		$balance = 0;
-		foreach($unspent as $utxo){
-			if($utxo['address'] == $address){
-				$balance += $utxo['amount'];
-			}
-		}
-		return $balance;
+		$unspent = $this->getaddressunspent($address, $min_conf);
+		return $unspent['total'];
 	}	
 	
 	public function updateutxoset()
 	{
-		self::$utxoset = $this->listunspent();
+		$get = $this->listunspent(0);
+		aasort($get, 'confirmations');
+		$get = array_reverse($get);
+		self::$utxoset = $get;
 		return self::$utxoset;
 	}
 	
-	public function getaddresstxlist($address, $level = 0)
+	public function getaddresstxlist($address, $level = 0, $limit = false)
 	{
-		//gets a full list of transactions involving this address, using blockchain.info
 		$output = array();
-		$limit = 50;
-		$offset = 0;
-		if($level > 0){
-			$offset = $level * $limit;
+		try{
+			$blocktrail = new BlocktrailSDK(BLOCKTRAIL_KEY, BLOCKTRAIL_SECRET, "BTC", BLOCKTRAIL_TESTNET);
+			$tx_list = $blocktrail->addressTransactions($address, $level, $limit, 'desc');
 		}
-		$url = 'https://blockchain.info/address/'.$address.'?format=json&limit='.$limit.'&offset='.$offset;
-		$get = @file_get_contents($url);
-		$decode = json_decode($get, true);
-		if($decode AND isset($decode['txs']) AND count($decode['txs']) > 0){
-			$tx_count = $decode['n_tx'];
-			$pages = ceil($tx_count / $limit);
-			
-			foreach($decode['txs'] as $tx){
+		catch(\Exception $e){
+			return array();
+		}
+
+		if($tx_list AND isset($tx_list['data']) AND count($tx_list['data']) > 0){
+			$tx_count = $tx_list['total'];
+			if(!$limit){
+				$pages = ceil($tx_count / $tx_list['per_page']);
+			}
+			foreach($tx_list['data'] as $tx){
 				$item = array();
 				$item['txId'] = $tx['hash'];
-				$item['time'] = $tx['time'];
-				$item['block'] = $tx['block_height'];
+				$item['time'] = strtotime($tx['time']);
 				$item['amount'] = 0;
+				$item['inputs'] = $tx['inputs'];
+				$item['outputs'] = $tx['outputs'];
 				foreach($tx['inputs'] as $input){
-					if($input['prev_out']['addr'] == $address){
-						$item['amount'] -= $input['prev_out']['value'];
+					if($input['address'] == $address){
+						$item['amount'] -= $input['value'];
 					}
 				}
-				foreach($tx['out'] as $out){
-					if($out['addr'] == $address){
+				foreach($tx['outputs'] as $out){
+					if(isset($out['address']) AND $out['address'] == $address){
 						$item['amount'] += $out['value'];
 					}
 				}
 				$output[] = $item;
 			}
 			
-			if($level == 0){
+			if($level == 0 AND !$limit){
 				$level_tx = array();
 				for($i = 1; $i <= $pages; $i++){
-					$level_tx = array_merge($level_tx, $this->getaddresstxlist($address, $i));
+					$level_tx = array_merge($level_tx, $this->getaddresstxlist($address, $i, $tx_list['per_page']));
 				}
 				$output = array_merge($output, $level_tx);
-				aasort($output, 'block');
-				$output = array_values($output);
+				aasort($output, 'time');
+				$output = array_reverse(array_values($output));
 			}
 		}
 		return $output;
 	}
-}
-?>
+	
+	public function getutxoset()
+	{
+		if(!self::$utxoset){
+			$this->updateutxoset();
+		}
+		return self::$utxoset;
+	}
+	
+	public function getaddressunspent($address, $min_conf = 1)
+	{
+		$unspent = $this->getutxoset();
+		$output = array('total' => 0, 'min_conf_total' => 0, 'count' => 0, 'utxos' => array());
+		foreach($unspent as $tx){
+			if($tx['confirmations'] < $min_conf){
+				continue;
+			}
+			if($tx['address'] == $address AND $tx['spendable'] == 1){
+				$output['total'] += $tx['amount'];
+				$output['count']++;
+				$output['utxos'][] = $tx;
+				if($tx['confirmations'] == $min_conf){
+					$output['min_conf_total'] += $tx['amount'];
+				}
+			}
+		}
+		return $output;
+	}
+	
+	public function getprimingfee($num, $per_input, $per_batch = 25)
+	{
+		$num_batches = ceil($num / $per_batch);
+		$batch_fee = ($per_batch * $this->min_fee) + ($this->min_fee * 3);
+		$total_batch_fee = $batch_fee * $num_batches;
+		$total_required = ($num * $per_input) + $total_batch_fee;
+		return $total_required;
+	}
+	
+	public function primeaddressinputs($address, $num, $per_input, $per_batch = 25, $use_stage = 1)
+	{
+		$unspent = $this->getaddressunspent($address);
+		$num_batches = ceil($num / $per_batch);
+		$total_required = round($this->getprimingfee($num, $per_input, $per_batch), 8);
+		if(round($unspent['total'], 8) < $total_required){
+			throw new \Exception('Not enough coin available for priming - need '.($total_required - $unspent['total']).' BTC more ('.$total_required.' total)'."\n");
+		}
+		$this->modify_request = false;			
+		if($num > $per_batch){
+			if($use_stage === 1){
+				//do "first stage priming" - create initial outputs for batch set of priming transactions
+				$batch_fee = (($per_input + $this->min_fee) * $per_batch) + $this->min_fee;
+				$total_required = ($batch_fee + $this->min_fee) * $num_batches;
+				$raw_outputs = array();
+				for($i = 0; $i < $num_batches; $i++){
+					$raw_outputs[] = array('address' => $address, 'value' => $batch_fee);
+				}
+				$stage = 1;
+			}
+			else{
+				//do second stage priming and create outputs for each batch
+				$total_needed = $per_input * $per_batch;
+				$output = array('stage' => 2, 'txs' => array());
+				for($i = 0; $i < $num_batches; $i++){	
+					$raw_outputs = array();
+					for($i2 = 0; $i2 < $per_batch; $i2++){
+						$raw_outputs[] = array('address' => $address, 'value' => $per_input);
+					}
 
+					$raw_inputs = array();
+					$total_used = 0;
+					foreach($unspent['utxos'] as $k => $tx){
+						$raw_inputs[] = array('txid' => $tx['txid'], 'vout' => $tx['vout']);
+						$total_used += $tx['amount'];
+						unset($unspent['utxos'][$k]);
+						if($total_used >= $total_needed){
+							break;
+						}
+					}
+					$total_needed += $this->min_fee;
+					$unused_amount = $total_used - $total_needed;
+					if($unused_amount > $this->dust_limit){
+						$raw_outputs[] = array('address' => $address, 'value' => $unused_amount);
+					}							
+					
+					$output['txs'][] = $this->sendcustomrawtransaction($raw_inputs, $raw_outputs);
+					sleep(2);
+					
+				}
+				return $output;
+			}
+
+		}
+		else{
+			//prime as normal, no need for batches
+			$raw_outputs = array(); 
+			for($i = 0; $i < $num; $i++){
+				$raw_outputs[] = array('address' => $address, 'value' => $per_input);
+			}
+			$stage = 2;
+		}
+		
+		$raw_inputs = array();
+		$total_used = 0;
+		foreach($unspent['utxos'] as $tx){
+			$raw_inputs[] = array('txid' => $tx['txid'], 'vout' => $tx['vout']);
+			$total_used += $tx['amount'];
+			if($total_used >= $total_required){
+				break;
+			}
+		}
+		
+		
+		$total_required += $this->min_fee;
+		$unused_amount = $total_used - $total_required;
+		if($unused_amount > $this->dust_limit){
+			$raw_outputs[] = array('address' => $address, 'value' => $unused_amount);
+		}		
+		
+		$output = $this->sendcustomrawtransaction($raw_inputs, $raw_outputs);
+		$output['stage'] = $stage;	
+		if($stage === 2){
+			$output = array('stage' => 2, 'txs' => array($output));
+		}
+		return $output;
+	}
+	
+	public function sendcustomrawtransaction($raw_inputs, $raw_outputs)
+	{
+		$create_raw = $this->bitwaspcreate($raw_inputs, $raw_outputs);
+		$this->walletpassphrase(XCP_WALLET, $this->wallet_timeout);
+		$sign = $this->signrawtransaction($create_raw);
+		$this->walletlock();
+		$broadcast = false;
+		if($sign['complete']){
+			$broadcast = $this->sendrawtransaction($sign['hex']);
+		}	
+		return array('inputs' => $raw_inputs, 'outputs' => $raw_outputs, 'raw' => $create_raw, 'signed' => $sign, 'txid' => $broadcast);	
+	}
+	
+	public function combineaddressutxos($address)
+	{
+		$unspent = $this->getaddressunspent($address);
+		if($unspent['count'] == 0){
+			return false;
+		}
+		$total_send = $unspent['total'] - ($this->min_fee * $unspent['count']);
+		$raw_inputs = array();
+		foreach($unspent['utxos'] as $tx){
+			$raw_inputs[] = array('txid' => $tx['txid'], 'vout' => $tx['vout']);
+		}
+		$raw_outputs[$address] = $total_send;
+		$create_raw = $this->bitwaspcreate($raw_inputs, $raw_outputs);
+		$this->walletpassphrase(XCP_WALLET, $this->wallet_timeout);
+		$sign = $this->signrawtransaction($create_raw);
+		$this->walletlock();
+		$broadcast = false;
+		if($sign['complete']){
+			$broadcast = $this->sendrawtransaction($sign['hex']);
+		}	
+		return array('inputs' => $raw_inputs, 'outputs' => $raw_outputs, 'raw' => $create_raw, 'signed' => $sign, 'txid' => $broadcast);	
+	}
+	
+	public function checkaddresspriming($address)
+	{
+		$unspent = $this->getaddressunspent($address, 0);
+		$is_priming = false;
+		foreach($unspent['utxos'] as $tx){
+			if($tx['confirmations'] == 0){
+				$get_raw = $this->getrawtransaction($tx['txid']);
+				$decode = $this->decoderawtransaction($get_raw);
+				$get_raw2 = $this->getrawtransaction($decode['vin'][0]['txid']);
+				$decode2 = $this->decoderawtransaction($get_raw2);
+				if(isset($decode2['vout'][0]['scriptPubKey']['addresses'][0])){
+					if($decode2['vout'][0]['scriptPubKey']['addresses'][0] == $address){
+						$is_priming = true;
+						break;
+					}
+				}
+			}
+		}
+		return $is_priming;
+	}
+	
+	
+	/**
+	* modified version of raw transaction creation from BitWasp library to allow for multiple outputs going to same address
+	*
+	*/
+    public static function bitwaspcreate($inputs, $outputs, $magic_byte = null, $magic_p2sh_byte = null)
+    {
+        $magic_byte = BitcoinLib::magicByte($magic_byte);
+        $magic_p2sh_byte = BitcoinLib::magicP2SHByte($magic_p2sh_byte);
+
+        $tx_array = array('version' => '1');
+
+        // Inputs is the set of [txid/vout/scriptPubKey]
+        $tx_array['vin'] = array();
+        foreach ($inputs as $input) {
+            if (!isset($input['txid']) || strlen($input['txid']) !== 64
+                || !isset($input['vout']) || !is_numeric($input['vout'])
+            ) {
+                return false;
+            }
+
+            $tx_array['vin'][] = array('txid' => $input['txid'],
+                'vout' => $input['vout'],
+                'sequence' => (isset($input['sequence'])) ? $input['sequence'] : 4294967295,
+                'scriptSig' => array('hex' => '')
+            );
+        }
+
+        // Outputs is the set of [address/amount]
+        $tx_array['vout'] = array();
+        foreach ($outputs as $k => $o) {
+			if(!is_array($o)){
+				$address = $k;
+				$value = $o;
+			}
+			else{
+				$address = $o['address'];
+				$value = $o['value'];	
+			}
+            if (!BitcoinLib::validate_address($address, $magic_byte, $magic_p2sh_byte)) {
+                return false;
+            }
+
+            $decode_address = BitcoinLib::base58_decode($address);
+            $version = substr($decode_address, 0, 2);
+            $hash = substr($decode_address, 2, 40);
+
+            if ($version == $magic_p2sh_byte) {
+                // OP_HASH160 <scriptHash> OP_EQUAL
+                $scriptPubKey = "a914{$hash}87";
+            } else {
+                // OP_DUP OP_HASH160 <pubKeyHash> OP_EQUALVERIFY OP_CHECKSIG
+                $scriptPubKey = "76a914{$hash}88ac";
+            }
+
+            $tx_array['vout'][] = array('value' => $value,
+                'scriptPubKey' => array('hex' => $scriptPubKey)
+            );
+        }
+
+        $tx_array['locktime'] = 0;
+
+        return RawTransaction::encode($tx_array);
+
+    }	
+	
+}
