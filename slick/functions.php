0a9fac2ab5f1a5df0310278591d8f16a0d1b9460
cryptonaut <nrathman@ironcladtech.ca>
2016-01-12 22:43:23 -0800

    added new helper function

--- slick/functions.php
+++ slick/functions.php
@@ -980,3 +980,22 @@ function remove_tags($str, $tags = array())
 	}
 	return $str;
 }
+
+function get_contents($str, $startDelimiter, $endDelimiter) {
+  $contents = array();
+  $startDelimiterLength = strlen($startDelimiter);
+  $endDelimiterLength = strlen($endDelimiter);
+  $startFrom = $contentStart = $contentEnd = 0;
+  while (false !== ($contentStart = strpos($str, $startDelimiter, $startFrom))) {
+    $contentStart += $startDelimiterLength;
+    $contentEnd = strpos($str, $endDelimiter, $contentStart);
+    if (false === $contentEnd) {
+      break;
+    }
+    $contents[] = substr($str, $contentStart, $contentEnd - $contentStart);
+    $startFrom = $contentEnd + $endDelimiterLength;
+  }
+
+  return $contents;
+}
+
