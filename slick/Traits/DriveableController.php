f9a3cd7557de0a46c30a07ce673f9905c365a15a
cryptonaut <nrathman@ironcladtech.ca>
2016-02-29 17:48:56 -0800

    made DriveableController trait replace class container

--- slick/Traits/DriveableController.php
+++ slick/Traits/DriveableController.php
@@ -9,7 +9,10 @@ trait DriveableController
 			throw new Exception('No driver key set for this module');
 		}
 		$driver = new Driver($this->driver_key, $this);		
-		$this->driver = $driver->load();		
+		$this->driver = $driver->load();
+		if(isset($this->container)){
+			$this->container = $this->driver;
+		}		
 		return $this->driver->init();
 	}	
 }
